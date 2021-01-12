89e9288d06bbbe20b92973bd463948866d526242
yvonnemorgun <74855275+yvonnemorgun@users.noreply.github.com>
2021-01-12 19:20:51 +0900

    .

--- app.py
+++ app.py
@@ -11,15 +11,12 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
-
 @app.route('/question')
 def question():
     page = request.args.get('page')
-    question = db.questions.find_one({'idx': int(page)}, {'_id': False})
-    answers = list(db.answers.find({'question_idx': int(page)}, {'_id': False}))
-    return jsonify({'result': 'success', 'question': question, "answers": answers})
-
+    question = db.questions.find_one({'idx':int(page)}, {'_id': False})
+    answers = list(db.answers.find({'question_idx':int(page)}, {'_id': False}))
+    return jsonify({'result': 'success','question': question, "answers":answers})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
-
