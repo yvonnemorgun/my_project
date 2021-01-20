775d296efe49a388146d1234a8f08c5d9ac29b05
yvonnemorgun <74855275+yvonnemorgun@users.noreply.github.com>
2021-01-20 20:19:42 +0900

    v

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def result(uuid):
     return render_template('result.html', sentence=sentence)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
