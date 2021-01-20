20461a375ef18689c33bb42a85dbcb79ec7100b3
yvonnemorgun <74855275+yvonnemorgun@users.noreply.github.com>
2021-01-20 19:44:16 +0900

    v

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+import uuid
 
 app = Flask(__name__)
 
@@ -18,10 +19,35 @@ def question():
     answers = list(db.answers.find({'question_idx':int(page)}, {'_id': False}))
     return jsonify({'result': 'success', 'question': question, "answers":answers})
 
-@app.route('/answers' methods=['GET'])
-def lastanswers():
+@app.route('/addAnswer')
+def addAnswer():
+    page = request.args.get('page')
+    addAnswer = db.questions.find({'idx': int(page)}, {'_id': False})
     answers = list(db.answers.find({'desc': int(page)}, {'_id': False}))
     return jsonify({'result': 'success', "answers": answers})
+
+@app.route('/makeAnswer')
+def makeAnswer():
+    page = request.args.get('page')
+
+@app.route('/save', methods=["POST"])
+def save():
+    idxs = request.form.getlist('answer_idxs[]')
+    uuid_value= str(uuid.uuid1())
+    result = {
+        "uuid":uuid_value,
+        "answer_idxs":idxs
+    }
+    db.result.insert_one(result)
+    return jsonify({'result': 'success', 'uuid':uuid_value})
+
+@app.route('/<uuid>', methods=["GET"])
+def result(uuid):
+    answer_idxs = db.result.find_one({"uuid":uuid})["answer_idxs"]
+    sentence = ""
+    for i in answer_idxs:
+        sentence += db.answers.find_one({"answer_idx":int(i)})["desc"]
+    return render_template('result.html', sentence=sentence)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
